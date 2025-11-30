fb06f7be1a85be00e89a64d638fd0393862233bc
Bryan Wang <bryanwang105@gmail.com>
2025-11-29 23:12:00 -0800

    Modified world settings, and added basic hotbar

--- Scenes/Player/UI/ToolHotbar.gd
+++ Scenes/Player/UI/ToolHotbar.gd
@@ -1,1 +1,24 @@
 extends PanelContainer
+
+@onready var panel: Panel = $MarginContainer/HBoxContainer/Panel
+@onready var panel_2: Panel = $MarginContainer/HBoxContainer/Panel2
+@onready var panel_3: Panel = $MarginContainer/HBoxContainer/Panel3
+@onready var panel_4: Panel = $MarginContainer/HBoxContainer/Panel4
+
+func change_slot(index):
+	print("INDEX")
+	print(index)
+	panel.modulate = Color.WHITE
+	panel_2.modulate = Color.WHITE
+	panel_3.modulate = Color.WHITE
+	panel_4.modulate = Color.WHITE
+	
+	match index:
+		0:
+			panel.modulate = Color.BLACK
+		1:
+			panel_2.modulate = Color.BLACK
+		2:
+			panel_3.modulate = Color.BLACK
+		3:
+			panel_4.modulate = Color.BLACK
--- Scenes/Player/tool_selector.gd
+++ Scenes/Player/tool_selector.gd
@@ -1,14 +1,16 @@
 extends Node
 
+@onready var hotbar: PanelContainer = $"../CanvasLayer/Control/Hotbar"
+
 var tools = [null, null, null, null]
-var active_index
+var active_index = 0
 
 func change_index(index):
 	active_index = index
+	hotbar.change_slot(index)
 
 func active_tool():
 	tools[active_index].activate()
-	
 
 func add_tool(tool_obj):
 	pass
@@ -16,6 +18,21 @@ func add_tool(tool_obj):
 func remove_tool(index):
 	tools[index] = null
 
-
 func replace_tool(index, tool_obj):
 	tools[index] = tool_obj
+
+# ------------------------------------------------------------------------------
+# INPUT HANDLING
+# ------------------------------------------------------------------------------
+func _input(event: InputEvent) -> void:
+	"""Handle 1, 2, 3 input."""
+	# Toggle pause/mouse capture
+	if Input.is_action_just_pressed("1"):
+		change_index(0)
+	elif Input.is_action_just_pressed("2"):
+		change_index(1)
+	elif Input.is_action_just_pressed("3"):
+		change_index(2)
+	elif Input.is_action_just_pressed("4"):
+		change_index(3)
+	
